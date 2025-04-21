5b1620b76b2ed7a20ed1a3505b7e15a76308bb70
muhimn <muhimn20122012@gmail.com>
2025-04-21 13:02:18 +0000

    Initial commit from GitHub Codespaces

--- index.js
+++ index.js
@@ -1,55 +1,43 @@
-const Discord = require('discord.js-selfbot-v13');
-const client = new Discord.Client({
-  readyStatus: false,
-  checkUpdate: false
-});
-//environment
-require('dotenv').config()
 
-function formatTime() { 
-  const date = new Date();
-  const options = {
-    timeZone: 'America/New_York', 
-    hour12: true,
-    hour: 'numeric',
-    minute: 'numeric'
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
-const express = require("express")
-const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
-  res.send(`
-  <body>
-  <center><h1>Bot 24H ON!</h1></center
-  </body>`)
-});
-client.on('ready', async () => {
-  console.clear();
-  console.log(`${client.user.tag} - rich presence started!`);
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
-  const r = new Discord.RichPresence()
-    .setApplicationId('1265825059692609587')
-    .setType('PLAYING')
-    .setURL('https://www.twitch.tv/apparentlyjack_rl') 
-    .setState('Hey Nitro is here')
-    .setName('quaaxz')
-    .setDetails(`Nitro is now`)
-    .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1041035673118965772/1270521845841657907/image_2.webp?ex=66b4012d&is=66b2afad&hm=c0fa475d23f70fc777bcea2e70d9682a9aedf2d565ae549e64552ac303361d2b&=&format=webp&width=696&height=379') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Nitro') 
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1041035673118965772/1270522062095781990/checked.png?ex=66b40160&is=66b2afe0&hm=1413e3f740030479e77899e2e3bebeb05f97a80e7c0b828e6ec6e9012f86255d&=&format=webp&quality=lossless&width=768&height=768') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Small Text') 
-    .addButton('Google', 'https://google.com');
+// اذا تبغى تضيف توكن زياده روح حطها تحت عند التوكن بس مو اخر سطر قبل الاخير او الثاني وكتبها بكتبلك ياه تحت
+//'',
+const tokens = [
+  //'',
 
-  client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+      'MTM2MzQ2NTEzOTkxNTA2MzM5Ng.G3T1yX.Up-L7zU4NK7XziZGm7ZMe0VxI6UWoyuMZGjXj0',
+          '',
+              '',
+                  '',
+                      ''
+                      ];
 
-});
+                      function createBot(token) {
+                          const client = new Client({
+                                  intents: [GatewayIntentBits.Guilds]
+                                      });
 
-const mySecret = process.env['TOKEN'];
-client.login(mySecret);
+                                          client.once('ready', () => {
+                                                  console.log(`Logged in as ${client.user.tag}`);
+                                                          console.log(`code by Sanigo`);
+                                                                  console.log(`team wick studio`);
+
+                                                                  //هاذي الستريمنق تخلي كل بوتاتك الستريمنق ولا تنسى تعدل كلمة سانيقو وتحط الكلام الي تبغاها اذا ما تبيها احذفها
+                                                                          client.user.setActivity('اني ابن البصره احجي بزوديبزودي ', {
+                                                                                      type: ActivityType.Streaming,
+                                                                                                  url: 'https://www.twitch.tv/tramp_in_top' 
+                                                                                                          });
+                                                                                                              });
+
+                                                                                                              //لا تلمس الي تحت لا تخرب شي عشان ما اتضارب معك
+
+                                                                                                                  client.login(token).catch(error => {
+                                                                                                                          console.error(`Failed to login with token: ${token}. Error: ${error.message}`);
+                                                                                                                              });
+                                                                                                                              }
+
+                                                                                                                              tokens.forEach(token => {
+                                                                                                                                  createBot(token);
+                                                                                                                                  });
+                                                                            
